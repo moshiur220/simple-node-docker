c5510ac0eea775bd62602a1978becc2337977aef
Moshiur Rahman <moshiur.rahman@ekkbaz.com>
2023-03-04 11:46:29 +0600

    docker composer

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const createError = require("http-errors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-
+const PORT = process.env.PORT || 4000;
 app.get("/", async (req, res, next) => {
   res.send({ message: "Add volume is ti work 🐻" });
 });
@@ -26,5 +27,29 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+// database connection
+console.log("Connecting to");
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB,
+} = process.env;
+
+// const uri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@$mongo:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const uri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+    app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
